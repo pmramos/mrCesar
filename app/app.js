d19781dee8e20c1749b53034397fee3689a911db
Jason Black <makotothedragon@gmail.com>
2015-03-29 14:20:05 -0400

    Shift over home.html to be what we're going with for the demo.

--- app/app.js
+++ app/app.js
@@ -9,7 +9,6 @@ angular.module('myApp', [
   'myApp.deadlines',
   'myApp.selectedList',
   'myApp.version',
-  'myApp.homePanel',
   'ui.bootstrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
--- app/home/home.js
+++ app/home/home.js
@@ -7,4 +7,17 @@ angular.module('myApp.home', ['ngRoute'])
     templateUrl: 'home/home.html',
     controller: 'HomeCtrl'
   });
+}])
+
+.controller('HomeCtrl', ['$scope', '$rootScope', function(scope, rootScope) {
+  scope.hasDeadline = false;
+  if(rootScope) {
+    if (rootScope.mainCollegeList) {
+      scope.hasDeadline = true;
+      scope.collegeDeadline = {"text": "Early Deadline 1", "date": rootScope.mainCollegeList[0]['edI']}
+    } else {
+      scope.hasDeadline = false;
+      scope.collegeDeadline = {"text": "No deadlines coming up"}
+    }
+  }
 }]);
